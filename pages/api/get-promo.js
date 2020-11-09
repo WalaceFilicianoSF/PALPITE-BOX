3241a4500d031eaf1f189fedcf55fb2dec03a3f4
WalaceFilicianoSF <67978899+WalaceFilicianoSF@users.noreply.github.com>
2020-11-09 15:14:10 -0300

    Update get-promo.js
    save.

--- pages/api/get-promo.js
+++ pages/api/get-promo.js
@@ -7,16 +7,16 @@ export default async(req, res) => {
     try{
     await doc.useServiceAccountAuth({
       client_email: process.env.SHEET_CLIENT_EMAIL,
-      private_key:fromBase64( process.env.SHEET_PRIVATE_KEY )
+      private_key:fromBase64(process.env.SHEET_PRIVATE_KEY )
     })
     await doc.loadInfo()
     
   
     const sheet = doc.sheetsByIndex[2]
-    await sheet.loadCells('A2:B2')  
+    await sheet.loadCells('A3:B3')  
     
-    const mostrarPromocaocell = sheet.getCell(1, 0)    
-    const textoCell = sheet.getCell(1, 1)
+    const mostrarPromocaocell = sheet.getCell(2, 0)    
+    const textoCell = sheet.getCell(2, 1)
     
 
     res.end(JSON.stringify({
